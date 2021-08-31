e21c78352bcf566b6a7e92472989e62f7afbc98b
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro-2.local>
2021-08-31 18:27:55 +0100

    changes

--- my_functions.py
+++ my_functions.py
@@ -47,8 +47,8 @@ chickens = [
   { "name": "Margaret", "age": 2, "eggs": 0 },
   { "name": "Hetty", "age": 1, "eggs": 2 },
   { "name": "Henrietta", "age": 3, "eggs": 1 },
-  { "name": "Audrey", "age": 2, "eggs": 0 },
-  { "name": "Mabel", "age": 5, "eggs": 1 },
+  { "name": "Audrey", "age": 2, "eggs": 4 },
+  { "name": "Mabel", "age": 5, "eggs": 3},
 ]
 
 def count_eggs(list):
